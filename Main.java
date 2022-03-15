8881664fc6220d33b90010e4386bd4e7b26119ae
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-03-15 12:02:27 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) 
+  {
+    double x;
+    double y;
+    Scanner a = new Scanner(System.in);
+    x=a.nextDouble();
+    Scanner b = new Scanner(System.in);
+    y=b.nextDouble();
+    System.out.println(x+y);
+}
+}
